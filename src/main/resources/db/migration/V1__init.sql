7ba4c5eea806b0a09ef01e091e7f474f71d8b346
yacooler <yacooler@yandex.ru>
2021-01-08 22:48:12 +0300

    Initial commit

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,28 @@
+create table product(
+    id bigserial primary key,
+    title varchar(255) not null,
+    price numeric(12,2) not null
+);
+
+insert into product(title, price)
+values
+('CPU AMD Ryzen 7', 17300),
+('CPU AMD Ryzen 5', 12400),
+('CPU Intel Celeron', 17300),
+('CPU Intel Core i3', 24900),
+('CPU Intel Core i5', 31200),
+('CPU Intel XEON 4 ядра', 38100),
+('CPU Intel XEON 6 ядер', 42100),
+('CPU Intel XEON 12 ядер', 75600),
+('Motherboard Asus AMD', 5900),
+('Motherboard Gigabyte AMD', 8300),
+('Motherboard Asus Intel', 6300),
+('Motherboard Noname Intel XEON', 7200),
+('RAM DDR3 4*2 GB', 4300),
+('RAM DDR3 8*2 GB', 9200),
+('RAM DDR4 8*2 GB', 12100),
+('RAM DDR4 16*2 GB', 17300),
+('Palit RTX 3060', 57200),
+('MSI RTX 3060ti', 63600),
+('Gigabyte RTX 3070', 65200),
+('Gigabyte RTX 3080', 102000);
